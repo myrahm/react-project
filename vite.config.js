c8cfa8090c9dccc7a7fb21e79814b49438260fa2
Myrah Mohammed <myrah.mohammed7@gmail.com>
2025-11-28 12:20:33 -0500

    Updated package.json scripts and vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-project/", // added this line for github pages deployment
   plugins: [react()],
 })
